8417414bc1b800bce69263c8627e21c3b0815e0c
Alex <asergy34@gmail.com>
2019-11-20 18:41:20 +0300

    Add validate form

--- src/components/routes/AuthPage.js
+++ src/components/routes/AuthPage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+import { Route, NavLink } from 'react-router-dom'
 
 import SignInForm from '../auth/SignInForm'
+import SignUpForm from '../auth/SignUpForm'
 
 class AuthPage extends Component {
 
@@ -8,10 +10,16 @@ class AuthPage extends Component {
         return (
             <div>
                 <h1>AuthPage</h1>
-                <SignInForm />
+                <NavLink to='/auth/signin' activeStyle={{ color: 'red' }}>Sign In</NavLink>
+                <NavLink to='/auth/signup' activeStyle={{ color: 'red' }}>Sign Up</NavLink>
+                <Route path='/auth/signin' render={ () => <SignInForm onSubmit={ this.handleSignIn } /> } />
+                <Route path='/auth/signup' render={ () => <SignUpForm onSubmit={ this.handleSignUp } /> } />
             </div>
         )
     }
+
+    handleSignIn = values => console.log('------', values);
+    handleSignUp = values => console.log('------', values);
 }
 
 export default AuthPage
