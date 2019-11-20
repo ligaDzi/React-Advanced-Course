ab307f179cd3d90c429a9351b336dfb03cc11bc4
Alex <asergy34@gmail.com>
2019-11-20 12:14:40 +0300

    Initial congig

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import Root from './components/Root'
+import store from './redux'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import history from './history'
 
 function App() {
   return (
-    <div>
-      Hello 
-    </div>
+    <Provider store = {store}>
+      <ConnectedRouter store = {store} history = {history}>
+        <Root />
+      </ConnectedRouter>
+    </Provider>
   );
 }
 
